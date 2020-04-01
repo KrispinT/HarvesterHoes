5dd1e18ef2ac2ab4fcedaa1305fea2af1b18cb3f
Krispy <48150088+Shipsterns@users.noreply.github.com>
2020-04-01 23:15:36 +1100

    Added ConsoleSender

--- HarvesterHoes/src/me/krispin/hoes/GiveHoeCommand.java
+++ HarvesterHoes/src/me/krispin/hoes/GiveHoeCommand.java
@@ -12,6 +12,48 @@ public class GiveHoeCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(!(sender instanceof Player)){
             //console command
+            if(args.length == 0){
+                sender.sendMessage(Utils.color("&cUsage: /harvesterhoe give <player> <amount>"));
+                return true;
+            }
+
+            if(args.length == 1){
+                sender.sendMessage(Utils.color("&cUsage: /harvesterhoe give <player> <amount>"));
+                return true;
+            }
+
+            if(args.length == 3){
+                Player target = Bukkit.getPlayer(args[1]);
+
+                if(target != null && target.isOnline()){
+                    try{
+
+                        int number = Integer.valueOf(args[2]);
+
+                        for(int i = 0; number > i; i++){
+                            target.getInventory().addItem(Utils.getHoe());
+
+                        }
+                        if(number == 1){
+                            sender.sendMessage(Utils.color("&aYou have given &2" + target.getName() + " &aa &6Harvester Hoe" + "&a."));
+                            target.sendMessage(Utils.color("&aYou have been given a &6Harvester Hoe &aby &2" + sender.getName() + "&a."));
+                            return true;
+                        }else{
+                            sender.sendMessage(Utils.color("&aYou have given &2" + target.getName() + " &a" + number + " &6Harvester Hoes" + "&a."));
+                            target.sendMessage(Utils.color("&aYou have been given " + number + " &6Harvester Hoes &aby &2" + sender.getName() + "&a."));
+                            return true;
+                        }
+
+                    }catch (NumberFormatException e){
+                        sender.sendMessage(Utils.color("Please use digits for the amount!"));
+                        e.printStackTrace();
+                        return true;
+                    }
+                }else{
+                    sender.sendMessage(Utils.color("&cCould not find player!"));
+                    return true;
+                }
+            }
         }else{
             //in-game command
 
